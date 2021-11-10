e682e86b24e9730861ec5a35c8a63252cc9f53b7
Jon Le Marquand <jon.lemarquand@purrgroup.com>
2021-11-10 20:01:53 +0000

    Initial setup

---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>The Homepage</h1>
+</template>
+
+<script>
+export default {
+    name: "Homepage"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/_variables.scss';
+ h1 {
+     color: $dull-color;
+ }
+</style>
